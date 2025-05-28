50cbbcb68e196367143a9de2af2d9afbe884ba6e
Cyrius <tom.archimbaud27@gmail.com>
2025-05-28 10:08:22 +0200

    Create tripleursShuffle.js

---
+++ javascript/tripleursShuffle.js
@@ -0,0 +1,17 @@
+<script>
+  function setRandomImage() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let url = "url('/assets/images/tripleurs" + randomNumber + ".png')";
+    let element = document.getElementById("tripleurs");
+    if (element) {
+      element.style.backgroundImage = url;
+      console.log("tripleur choisi = " + url);
+    }
+  }
+
+  // Initial load
+  document.addEventListener("DOMContentLoaded", setRandomImage);
+
+  // Swup page transition
+  document.addEventListener("swup:contentReplaced", setRandomImage);
+</script>
